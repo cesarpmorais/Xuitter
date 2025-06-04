7447b9a8385f4e3afa04ec4977265cc8c7aa39e6
Matheus Grandinetti <matheusgrandinetti@gmail.com>
2025-06-04 08:35:10 -0300

    feed route added

--- xuitter-backend/post/urls.py
+++ xuitter-backend/post/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from post.views import PostView, PostActionView
+from post.views import PostView, PostActionView, FeedView
 
 urlpatterns = [
     path('', PostView.as_view(), name='post'),
     path('<int:pk>/', PostView.as_view(), name='post-detail'),
-    path('<int:pk>/action/', PostActionView.as_view(), name='post-action')
+    path('<int:pk>/action/', PostActionView.as_view(), name='post-action'),
+    path('feed/', FeedView.as_view(), name='feed')
 ]
